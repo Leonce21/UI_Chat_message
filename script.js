37d559c4f04ffb2c43aec44650316bd8420ada86
leonce2 <leoncefotso59@gmail.com>
2023-10-09 12:40:43 +0100

    add emoticons

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ icon.onclick = function(){
    }
 }
 
+$(document).ready(function() {
+   // Initialize emojioneArea on the input field
+   $("#emoji-input").emojioneArea({
+       pickerPosition: "top", // Position the emoji picker above the input field
+       events: {
+           // Add a click event listener to the emoji picker
+           click: function (picker, emoji) {
+               // Insert the selected emoji into the input field
+               $("#emoji-input").val($("#emoji-input").val() + emoji.shortname);
+           }
+       }
+   });
+});
